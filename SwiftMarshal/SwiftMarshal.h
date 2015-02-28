68ab666d988f3ca964aa0519b776f9883c9e977d
Jonas Zaugg <jonas.zaugg@yahoo.com>
2015-02-28 02:54:24 +0100

    Initial Commit

---
+++ SwiftMarshal/SwiftMarshal.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMarshal.h
+//  SwiftMarshal
+//
+//  Created by Jonas Zaugg on 28.02.15.
+//  Copyright (c) 2015 Jonas Zaugg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMarshal.
+FOUNDATION_EXPORT double SwiftMarshalVersionNumber;
+
+//! Project version string for SwiftMarshal.
+FOUNDATION_EXPORT const unsigned char SwiftMarshalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMarshal/PublicHeader.h>
+
+
